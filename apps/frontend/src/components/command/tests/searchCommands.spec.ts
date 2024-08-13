36242ff08a600d282522abfdacd1cdf7213c7e83
TTiip <2711430287@qq.com>
2024-08-13 17:24:48 +0800

    feat: 11-测试 searchCommands

--- apps/frontend/src/components/command/tests/searchCommands.spec.ts
+++ apps/frontend/src/components/command/tests/searchCommands.spec.ts
@@ -17,12 +17,12 @@ describe('搜索命令', () => {
 		})
 	})
 
+	// 每次请求之前都reset commands 的列表值
 	beforeEach(() => {
 		const { resetSearchCommands } = useSearchCommands()
 		resetSearchCommands()
 	})
 
-	// 每次请求之前都reset commands 的列表值
 	test('搜索不存在的命令', () => {
 		const { searchCommands, filteredCommands } = useSearchCommands()
 		searchCommands('不存在的主页')
