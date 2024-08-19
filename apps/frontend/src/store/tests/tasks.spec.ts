3cd10f7d099d873cb220a4b53931818147ccb5b0
TTiip <2711430287@qq.com>
2024-08-19 17:39:36 +0800

    feat: 14-测试 tasks store(2)

--- apps/frontend/src/store/tests/tasks.spec.ts
+++ apps/frontend/src/store/tests/tasks.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test, vi, beforeEach } from "vitest"
 import { TaskStatus, useTasksStore } from "../tasks"
 import { liveListProject } from "@/tests/fixture"
-import { fetchCreateTask } from "@/api"
+import { fetchCompleteTask, fetchCreateTask, fetchMoveTaskToProject, fetchRemoveTask, fetchRestoreTask } from "@/api"
 import { createPinia, setActivePinia } from "pinia"
 import { useTasksSelectorStore } from "../tasksSelector"
 import { completeSmartProject } from "../smartProjects"
@@ -20,16 +20,20 @@ vi.mocked(fetchCreateTask).mockImplementation(async (title: string) => {
 	}
 })
 
+beforeEach(() => {
+	setActivePinia(createPinia())
+
+	const taskSelectorStore = useTasksSelectorStore()
+	taskSelectorStore.currentSelector = liveListProject
+
+	// 重置 mock
+	// vi.mocked(fetchCreateTask).mockClear()
+	// 重置所有的 mock
+	vi.clearAllMocks()
+})
+
 describe('tasks store', () => {
 	describe('添加任务', () => {
-		beforeEach(() => {
-			setActivePinia(createPinia())
-
-			// 重置 mock
-			// vi.mocked(fetchCreateTask).mockClear()
-			// 重置所有的 mock
-			vi.clearAllMocks()
-		})
 
 		test('添加task 当前的 currentActiveTask 是一个 undefined 的时候不应该添加', async () => {
 			const taskSelectorStore = useTasksSelectorStore()
@@ -60,10 +64,7 @@ describe('tasks store', () => {
 		})
 
 		test('添加task 应当在列表的最前面', async () => {
-			const taskSelectorStore = useTasksSelectorStore()
-
 			const taskStore = useTasksStore()
-			taskSelectorStore.currentSelector = liveListProject
 
 			await taskStore.addTask('吃饭')
 			const task1 = await taskStore.addTask('运动')
@@ -73,6 +74,72 @@ describe('tasks store', () => {
 			expect(taskStore.tasks[0]).toEqual(task1)
 			expect(taskStore.currentActiveTask).toEqual(task1)
 			expect(fetchCreateTask).toBeCalledWith(task1?.title, liveListProject.id)
+		})
+	})
+
+	describe('删除任务', () => {
+		test('删除任务', async () => {
+			const taskStore = useTasksStore()
+			const task = (await taskStore.addTask('吃饭'))!
+
+			await taskStore.removeTask(task)
+
+			expect(taskStore.tasks.length).toBe(0)
+			expect(taskStore.currentActiveTask).toBeUndefined()
+			expect(fetchRemoveTask).toBeCalledWith(task?.id)
+		})
+	})
+
+	describe('完成任务', () => {
+		test('完成任务', async () => {
+			const taskStore = useTasksStore()
+			const task = (await taskStore.addTask('吃饭'))!
+
+			await taskStore.completeTask(task)
+
+			expect(taskStore.tasks.length).toBe(0)
+			expect(taskStore.currentActiveTask).toBeUndefined()
+			expect(fetchCompleteTask).toBeCalledWith(task?.id)
+		})
+	})
+
+	describe('恢复完成任务', () => {
+		test('恢复完成任务', async () => {
+			const taskStore = useTasksStore()
+			const task = (await taskStore.addTask('吃饭'))!
+
+			await taskStore.restoreTask(task)
+
+			expect(taskStore.tasks.length).toBe(0)
+			expect(taskStore.currentActiveTask).toBeUndefined()
+			expect(fetchRestoreTask).toBeCalledWith(task?.id)
+		})
+	})
+
+	describe('恢复完成任务', () => {
+		test('恢复完成任务', async () => {
+			const taskStore = useTasksStore()
+			const task = (await taskStore.addTask('吃饭'))!
+
+			await taskStore.restoreTask(task)
+
+			expect(taskStore.tasks.length).toBe(0)
+			expect(taskStore.currentActiveTask).toBeUndefined()
+			expect(fetchRestoreTask).toBeCalledWith(task?.id)
+		})
+	})
+
+	describe('移动任务', () => {
+		test('移动任务', async () => {
+			const taskStore = useTasksStore()
+			const task = (await taskStore.addTask('吃饭'))!
+
+			const ProjectId = 'xx'
+			await taskStore.moveTaskToProject(task, ProjectId)
+
+			expect(taskStore.tasks.length).toBe(0)
+			expect(taskStore.currentActiveTask).toBeUndefined()
+			expect(fetchMoveTaskToProject).toBeCalledWith(task?.id, ProjectId)
 		})
 	})
 })
