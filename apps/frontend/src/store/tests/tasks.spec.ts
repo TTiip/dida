fc5908d9ded15394f137022d11907aeb0026151e
TTiip <2711430287@qq.com>
2024-08-19 18:22:58 +0800

    feat: 15-测试 tasks store(3)

--- apps/frontend/src/store/tests/tasks.spec.ts
+++ apps/frontend/src/store/tests/tasks.spec.ts
@@ -1,16 +1,20 @@
 import { describe, expect, test, vi, beforeEach } from "vitest"
 import { TaskStatus, useTasksStore } from "../tasks"
 import { liveListProject } from "@/tests/fixture"
-import { fetchCompleteTask, fetchCreateTask, fetchMoveTaskToProject, fetchRemoveTask, fetchRestoreTask } from "@/api"
+import { fetchAllTasks, fetchCompleteTask, fetchCreateTask, fetchMoveTaskToProject, fetchRemoveTask, fetchRestoreTask } from "@/api"
 import { createPinia, setActivePinia } from "pinia"
 import { useTasksSelectorStore } from "../tasksSelector"
 import { completeSmartProject } from "../smartProjects"
 
 vi.mock('@/api')
-vi.mocked(fetchCreateTask).mockImplementation(async (title: string) => {
+vi.mocked(fetchCreateTask).mockImplementation(async (title: string) => createTaskResponse(title))
+// 不验证类型 直接提供一个 空数据 即可
+vi.mocked(fetchAllTasks).mockResolvedValue([])
+
+function createTaskResponse (title: string) {
 	return {
-		title,
-		content: '这是 content',
+		title: title,
+		content: '这是内容',
 		status: TaskStatus.ACTIVE,
 		projectId: '1',
 		position: 1,
@@ -18,7 +22,16 @@ vi.mocked(fetchCreateTask).mockImplementation(async (title: string) => {
 		createdAt: new Date().toString(),
 		updatedAt: new Date().toString()
 	}
-})
+}
+
+function expectTaskDataStructure (task: any) {
+	expect(task.title).toBe('吃饭')
+	expect(task).toHaveProperty('id')
+	expect(task).toHaveProperty('content')
+	expect(task).toHaveProperty('status')
+	expect(task).toHaveProperty('position')
+	expect(task).toHaveProperty('projectId')
+}
 
 beforeEach(() => {
 	setActivePinia(createPinia())
@@ -127,6 +140,43 @@ describe('tasks store', () => {
 			expect(taskStore.tasks.length).toBe(0)
 			expect(taskStore.currentActiveTask).toBeUndefined()
 			expect(fetchMoveTaskToProject).toBeCalledWith(task?.id, ProjectId)
+		})
+	})
+
+	describe('修改任务', () => {
+		test('修改任务', async () => {
+			const taskStore = useTasksStore()
+			await taskStore.updateTasks([createTaskResponse('吃饭')])
+
+			expect(taskStore.tasks.length).toBe(1)
+			// ts 没有必要检测类型 编写的时候就会提示
+			// expectTaskDataStructure(taskStore.tasks[0])
+		})
+
+		test('根据id去修改 任务状态', async () => {
+			const taskStore = useTasksStore()
+			const task = (await taskStore.addTask('吃饭'))!
+			await taskStore.changeActiveTask(task.id)
+
+			expect(taskStore.currentActiveTask).toEqual(task)
+		})
+
+		test('根据任务本身去修改 任务状态', async () => {
+			const taskStore = useTasksStore()
+			const task = (await taskStore.addTask('吃饭'))!
+			await taskStore.changeActiveTask(task)
+
+			expect(taskStore.currentActiveTask).toEqual(task)
+		})
+	})
+
+	describe('搜索所有的未删除状态的任务', () => {
+		test('搜索所有的未删除状态的任务', async () => {
+			const taskStore = useTasksStore()
+			await taskStore.findAllTasksNotRemoved()
+
+			expect(fetchAllTasks).toBeCalledWith({ status: TaskStatus.ACTIVE })
+			expect(fetchAllTasks).toBeCalledWith({ status: TaskStatus.COMPLETED })
 		})
 	})
 })
